f14aec6621a60ed4057d4535bb8cc45d4dc80341
yadhukrishnasm <yadhukrishna.me@gmail.com>
2024-02-07 04:16:39 +0530

    navbar commited to user

---
+++ greenbin_client/greenbin_app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../styles/navbar.css'; 
+
+
+const Navbar = () => {
+  return (
+    <nav className="desktop-nav">   
+    <div className="container">
+      <a href="/" className="logo">
+          <b id="green">Green</b>bin
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
