d718a3e5eddf0ccf32945d9c94fba49c015302a1
yadhukrishnasm <yadhukrishna.me@gmail.com>
2024-02-07 10:15:40 +0530

    modified:   greenbin_client/greenbin_app/src/homepage.js 	modified:   greenbin_client/greenbin_app/src/styles/home.css

--- greenbin_client/greenbin_app/src/homepage.js
+++ greenbin_client/greenbin_app/src/homepage.js
@@ -3,15 +3,19 @@ import './styles/home.css'
 import { Link } from 'react-router-dom';
 export default function homepage() {
   return (
-    <div className='home-page'>
-        <p className='logo'>
-        <b>Green</b>bin
-        </p>
-        <div className="links">
-            <div ></div>
-            <Link to="/ServiceProvider" id='provider'>register as collector</Link>
-            <Link to="/Register" id='reg'>register as new User</Link>
-        </div>
-    </div>
+  
+      <div className='home-page'>
+          <p className='logo'>
+          <b>Green</b>bin
+          </p>
+          <div className="links-box">
+          
+                <Link to="/provider" className='links'>register as collector</Link>
+          
+                <Link to="/user" className='links'>register as new User</Link>
+          
+          </div>
+      </div>
+ 
   )
   }
