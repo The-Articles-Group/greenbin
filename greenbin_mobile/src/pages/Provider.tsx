4119992c204413223064cd7cfb5a31234098e612
deltrexgg <deltrexgg@gmail.com>
2024-02-07 10:54:36 +0530

    modified:   greenbin_mobile/src/pages/Provider.tsx

--- greenbin_mobile/src/pages/Provider.tsx
+++ greenbin_mobile/src/pages/Provider.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Provider = () => {
-  return <div>Provider</div>;
+  return <><div><h4>Name</h4>
+  <h5>Phone No</h5></div>
+  <div>
+    <div>
+      <span>Location</span>
+      <span>WardNO</span>
+      <span>Address</span>
+      <span>Name</span>
+      <span>Phone No</span>
+    </div>
+    </div></>
 };
 
 export default Provider;
