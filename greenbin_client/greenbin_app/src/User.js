4a0d44e787ce057ac1ed45898aa2f9184e0978ce
Savand Shyam Viswam <savandshyamviswam@gmail.com>
2024-02-07 09:23:57 +0530

    properly setup routes, and login page

---
+++ greenbin_client/greenbin_app/src/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RegisterUp from "./components/registerUp.js";
+import Navbar from "./components/Navbar.js";
+
+const User = () => {
+  return (
+    <div>
+      <Navbar />
+      <RegisterUp />
+    </div>
+  );
+};
+
+export default User;
--- greenbin_client/greenbin_app/src/index.js
+++ greenbin_client/greenbin_app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowswerRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 // import App from './App';
 import Provider from "./ServiceProvider";
@@ -11,17 +11,43 @@ import "./App.css";
 
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase";
+import Login from "./Login";
+import AuthRoute from "./components/AuthRoute";
+import User from "./User";
 
 const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <HelmetProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <AuthRoute>
+                <Home />
+              </AuthRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/user"
+            element={
+              <AuthRoute>
+                <User />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/provider"
+            element={
+              <AuthRoute>
+                <Provider />
+              </AuthRoute>
+            }
+          />
         </Routes>
       </Router>
     </HelmetProvider>
