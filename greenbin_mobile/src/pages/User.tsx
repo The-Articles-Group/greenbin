29785c94be2854b8c6f97d1ca0fb2ced51ffbfe0
deltrexgg <deltrexgg@gmail.com>
2024-02-07 10:49:33 +0530

    modified:   greenbin_mobile/src/pages/User.tsx

--- greenbin_mobile/src/pages/User.tsx
+++ greenbin_mobile/src/pages/User.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const User = () => {
-  return <div>User</div>;
+
+
+  return <div><h4>Name</h4>
+  <h5>Phone Number</h5> </div>;
+  <div>
+    <label>Amount of Bio waste</label><input type="number" placeholder="Sac amount"  /><br />
+    <label>Amount of Plastic waste</label><input type="number" /><br />
+    <label>Amount of Electronics waste</label><input type="number" /><br />
+    <span></span>
+    <button>Pick Up</button>
+  </div>
 };
 
 export default User;
